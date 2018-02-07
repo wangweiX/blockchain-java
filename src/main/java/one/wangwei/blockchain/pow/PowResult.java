9701196c1497e3b0a9821bdaf832018b94113fac
wangwei <w7years@gmail.com>
2018-02-07 11:30:07 +0800

    修改 PowResult 注释

--- src/main/java/one/wangwei/blockchain/pow/PowResult.java
+++ src/main/java/one/wangwei/blockchain/pow/PowResult.java
@@ -10,12 +10,13 @@ import lombok.Data;
  */
 @Data
 public class PowResult {
+
     /**
-     *
+     * 计数器
      */
     private long nonce;
     /**
-     *
+     * hash值
      */
     private String hash;
 
