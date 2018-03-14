2eaebe32fb1c098ca786b979cedc2e05366933b8
wangwei <w7years@gmail.com>
2018-03-14 23:50:15 +0800

    创建对象 Wallet

---
+++ src/main/java/one/wangwei/blockchain/wallet/Wallet.java
@@ -0,0 +1,18 @@
+package one.wangwei.blockchain.wallet;
+
+/**
+ * 钱包
+ *
+ * @author wangwei
+ * @date 2018/03/14
+ */
+public class Wallet {
+
+    /**
+     * 公钥
+     */
+    private byte[] publicKey;
+
+
+
+}
