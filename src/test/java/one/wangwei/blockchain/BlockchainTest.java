08fe2cc41d4883b3e45de5cbc7b51b95d6629e44
wangwei <w7years@gmail.com>
2018-04-15 19:56:03 +0800

    测试

--- src/test/java/one/wangwei/blockchain/BlockchainTest.java
+++ src/test/java/one/wangwei/blockchain/BlockchainTest.java
@@ -18,8 +18,8 @@ public class BlockchainTest {
             // 1G9TkDEp9YTnGa6gS5zaWkwGQwKrRykXcf
             // 1EKacQPNxTd8N7Y83VK11zoqm7bhUZiDHm
 //            String[] argss = {"printaddresses"};
-            String[] argss = {"getbalance", "-address", "1G9TkDEp9YTnGa6gS5zaWkwGQwKrRykXcf"};
-//            String[] argss = {"send", "-from", "1CceyiwYXh6vL6dLPw6WiNc5ihqVxwYHSA", "-to", "1EKacQPNxTd8N7Y83VK11zoqm7bhUZiDHm", "-amount", "5"};
+//            String[] argss = {"getbalance", "-address", "1G9TkDEp9YTnGa6gS5zaWkwGQwKrRykXcf"};
+            String[] argss = {"send", "-from", "1CceyiwYXh6vL6dLPw6WiNc5ihqVxwYHSA", "-to", "1EKacQPNxTd8N7Y83VK11zoqm7bhUZiDHm", "-amount", "5"};
             CLI cli = new CLI(argss);
             cli.parse();
         } catch (Exception e) {
