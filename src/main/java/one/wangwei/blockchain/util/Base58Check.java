00b67b277b0aca016215269222f9316021fbde8e
wangwei <w7years@gmail.com>
2018-07-02 11:00:54 +0800

    更新代码

--- src/main/java/one/wangwei/blockchain/util/Base58Check.java
+++ src/main/java/one/wangwei/blockchain/util/Base58Check.java
@@ -5,7 +5,6 @@
  * https://www.nayuki.io/page/bitcoin-cryptography-library
  * https://github.com/nayuki/Bitcoin-Cryptography-Library
  */
-
 package one.wangwei.blockchain.util;
 
 import java.io.ByteArrayOutputStream;
@@ -13,20 +12,27 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
 
-
 /**
- * Converts between an array of bytes and a Base58Check string. Not instantiable.
+ * Base58 转化工具
  */
 public final class Base58Check {
 
-    /*---- Static functions ----*/
-
-    // Adds the checksum and converts to Base58Check. Note that the caller needs to prepend the version byte(s).
+    /**
+     * 添加校验码并转化为 Base58 字符串
+     *
+     * @param data
+     * @return
+     */
     public static String bytesToBase58(byte[] data) {
         return rawBytesToBase58(addCheckHash(data));
     }
 
-    // Directly converts to Base58Check without adding a checksum.
+    /**
+     * 转化为 Base58 字符串
+     *
+     * @param data
+     * @return
+     */
     public static String rawBytesToBase58(byte[] data) {
         // Convert to base-58 string
         StringBuilder sb = new StringBuilder();
@@ -38,16 +44,22 @@ public final class Base58Check {
         }
 
         // Add '1' characters for leading 0-value bytes
-        for (int i = 0; i < data.length && data[i] == 0; i++)
+        for (int i = 0; i < data.length && data[i] == 0; i++) {
             sb.append(ALPHABET.charAt(0));
+        }
         return sb.reverse().toString();
     }
 
 
-    // Returns a new byte array by concatenating the given array with its checksum.
+    /**
+     * 添加校验码并返回待有校验码的原生数据
+     *
+     * @param data
+     * @return
+     */
     static byte[] addCheckHash(byte[] data) {
         try {
-            byte[] hash = Arrays.copyOf(Sha256.getDoubleHash(data).toBytes(), 4);
+            byte[] hash = Arrays.copyOf(BtcAddressUtils.doubleHash(data), 4);
             ByteArrayOutputStream buf = new ByteArrayOutputStream();
             buf.write(data);
             buf.write(hash);
@@ -58,41 +70,53 @@ public final class Base58Check {
     }
 
 
-    // Converts the given Base58Check string to a byte array, verifies the checksum, and removes the checksum to return the payload.
-    // The caller is responsible for handling the version byte(s).
+    /**
+     * 将 Base58Check 字符串转化为 byte 数组，并校验其校验码
+     * 返回的byte数组带有版本号，但不带有校验码
+     *
+     * @param s
+     * @return
+     */
     public static byte[] base58ToBytes(String s) {
         byte[] concat = base58ToRawBytes(s);
         byte[] data = Arrays.copyOf(concat, concat.length - 4);
         byte[] hash = Arrays.copyOfRange(concat, concat.length - 4, concat.length);
-        byte[] rehash = Arrays.copyOf(Sha256.getDoubleHash(data).toBytes(), 4);
-        if (!Arrays.equals(rehash, hash))
+        byte[] rehash = Arrays.copyOf(BtcAddressUtils.doubleHash(data), 4);
+        if (!Arrays.equals(rehash, hash)) {
             throw new IllegalArgumentException("Checksum mismatch");
+        }
         return data;
     }
 
 
-    // Converts the given Base58Check string to a byte array, without checking or removing the trailing 4-byte checksum.
+    /**
+     * 将 Base58Check 字符串反转为 byte 数组
+     *
+     * @param s
+     * @return
+     */
     static byte[] base58ToRawBytes(String s) {
         // Parse base-58 string
         BigInteger num = BigInteger.ZERO;
         for (int i = 0; i < s.length(); i++) {
             num = num.multiply(ALPHABET_SIZE);
             int digit = ALPHABET.indexOf(s.charAt(i));
-            if (digit == -1)
+            if (digit == -1) {
                 throw new IllegalArgumentException("Invalid character for Base58Check");
+            }
             num = num.add(BigInteger.valueOf(digit));
         }
-
         // Strip possible leading zero due to mandatory sign bit
         byte[] b = num.toByteArray();
-        if (b[0] == 0)
+        if (b[0] == 0) {
             b = Arrays.copyOfRange(b, 1, b.length);
-
+        }
         try {
             // Convert leading '1' characters to leading 0-value bytes
             ByteArrayOutputStream buf = new ByteArrayOutputStream();
-            for (int i = 0; i < s.length() && s.charAt(i) == ALPHABET.charAt(0); i++)
+            for (int i = 0; i < s.length() && s.charAt(i) == ALPHABET.charAt(0); i++) {
                 buf.write(0);
+            }
             buf.write(b);
             return buf.toByteArray();
         } catch (IOException e) {
